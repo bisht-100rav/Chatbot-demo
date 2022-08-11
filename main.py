98045c3921aea672611be1fc49b9653505a756ca
Saurav Bisht <39938569+bisht-100rav@users.noreply.github.com>
2022-08-11 18:21:49 -0400

    added

---
+++ main.py
@@ -0,0 +1,447 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 16:48:12 2022
+
+@author: Saurav
+"""
+
+from tkinter import *
+import time
+import tkinter.messagebox
+import re
+#import nltk
+
+window_size="400x400"
+
+
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+    
+# Creating bag of words
+############################################################################################
+    
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+############################################################################################################################
+
+
+class ChatInterface(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        # sets default bg for top level windows
+        self.tl_bg = "#EEEEEE"
+        self.tl_bg2 = "#EEEEEE"
+        self.tl_fg = "#000000"
+        self.font = "Verdana 10"
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu, bd=5)
+        
+        
+    
+# Menu bar
+
+    # File
+        file = Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=file)
+       # file.add_command(label="Save Chat Log", command=self.save_chat)
+        file.add_command(label="Clear Chat", command=self.clear_chat)
+      #  file.add_separator()
+        file.add_command(label="Exit",command=self.chatexit)
+
+    # Options
+        options = Menu(menu, tearoff=0)
+        menu.add_cascade(label="Options", menu=options)
+
+        # username
+       
+
+        
+
+        # font
+        font = Menu(options, tearoff=0)
+        options.add_cascade(label="Font", menu=font)
+        font.add_command(label="Default",command=self.font_change_default)
+        font.add_command(label="Times",command=self.font_change_times)
+        font.add_command(label="System",command=self.font_change_system)
+        font.add_command(label="Helvetica",command=self.font_change_helvetica)
+        font.add_command(label="Fixedsys",command=self.font_change_fixedsys)
+
+        # color theme
+        color_theme = Menu(options, tearoff=0)
+        options.add_cascade(label="Color Theme", menu=color_theme)
+        color_theme.add_command(label="Default",command=self.color_theme_default) 
+       # color_theme.add_command(label="Night",command=self.) 
+        color_theme.add_command(label="Grey",command=self.color_theme_grey) 
+        color_theme.add_command(label="Blue",command=self.color_theme_dark_blue) 
+       
+        color_theme.add_command(label="Torque",command=self.color_theme_turquoise)
+        color_theme.add_command(label="Hacker",command=self.color_theme_hacker)
+       # color_theme.add_command(label='Mkbhd',command=self.MKBHD)
+
+
+      
+        help_option = Menu(menu, tearoff=0)
+        menu.add_cascade(label="Help", menu=help_option)
+        #help_option.add_command(label="Features", command=self.features_msg)
+        help_option.add_command(label="About PyBot", command=self.msg)
+        help_option.add_command(label="Develpoers", command=self.about)
+
+        self.text_frame = Frame(self.master, bd=6)
+        self.text_frame.pack(expand=True, fill=BOTH)
+
+        # scrollbar for text box
+        self.text_box_scrollbar = Scrollbar(self.text_frame, bd=0)
+        self.text_box_scrollbar.pack(fill=Y, side=RIGHT)
+
+        # contains messages
+        self.text_box = Text(self.text_frame, yscrollcommand=self.text_box_scrollbar.set, state=DISABLED,
+                             bd=1, padx=6, pady=6, spacing3=8, wrap=WORD, bg=None, font="Verdana 10", relief=GROOVE,
+                             width=10, height=1)
+        self.text_box.pack(expand=True, fill=BOTH)
+        self.text_box_scrollbar.config(command=self.text_box.yview)
+
+        # frame containing user entry field
+        self.entry_frame = Frame(self.master, bd=1)
+        self.entry_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+        # entry field
+        self.entry_field = Entry(self.entry_frame, bd=1, justify=LEFT)
+        self.entry_field.pack(fill=X, padx=6, pady=6, ipady=3)
+        # self.users_message = self.entry_field.get()
+
+        # frame containing send button and emoji button
+        self.send_button_frame = Frame(self.master, bd=0)
+        self.send_button_frame.pack(fill=BOTH)
+
+        # send button
+        self.send_button = Button(self.send_button_frame, text="Send", width=5, relief=GROOVE, bg='white',
+                                  bd=1, command=lambda: self.send_message_insert(None), activebackground="#FFFFFF",
+                                  activeforeground="#000000")
+        self.send_button.pack(side=LEFT, ipady=8)
+        self.master.bind("<Return>", self.send_message_insert)
+        
+        self.last_sent_label(date="No messages sent.")
+        #t2 = threading.Thread(target=self.send_message_insert(, name='t1')
+        #t2.start()
+        
+        
+    def last_sent_label(self, date):
+
+        try:
+            self.sent_label.destroy()
+        except AttributeError:
+            pass
+
+        self.sent_label = Label(self.entry_frame, font="Verdana 7", text=date, bg=self.tl_bg2, fg=self.tl_fg)
+        self.sent_label.pack(side=LEFT, fill=X, padx=3)
+
+    def clear_chat(self):
+        self.text_box.config(state=NORMAL)
+        self.last_sent_label(date="No messages sent.")
+        self.text_box.delete(1.0, END)
+        self.text_box.delete(1.0, END)
+        self.text_box.config(state=DISABLED)
+
+    def chatexit(self):
+        exit()
+
+    def msg(self):
+        tkinter.messagebox.showinfo("PyBOT v1.0",'PyBOT is a chatbot for answering python queries\nIt is based on retrival-based NLP using pythons NLTK tool-kit module\nGUI is based on Tkinter\nIt can answer questions regarding python language for new learners')
+
+    def about(self):
+        tkinter.messagebox.showinfo("PyBOT Developers","1.Abhishek Ezhava\n2.Mayur Kadam\n3.Monis Khot\n4.Raj Vishwakarma")
+    
+    def send_message_insert(self, message):
+         
+            # Takes the user input and converts all characters to lowercase
+        user_input = self.entry_field.get()
+        user_input = user_input.lower()
+        pr1 = "Human : " + user_input + "\n"
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, pr1)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+
+            # Defining the Chatbot's exit condition
+
+        if user_input == 'quit' or user_input == 'exit': 
+                
+                # response
+            ob="Thank you for visiting.)"
+            pr="PyBot : " + ob + "\n"
+            self.text_box.configure(state=NORMAL)
+            self.text_box.insert(END, pr)
+            self.text_box.configure(state=DISABLED)
+            self.text_box.see(END)
+            
+        
+        results = model.predict([bag_of_words(user_input, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+    
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                response = tg['responses']
+                ob = random.choice(response)
+                break
+            else:
+                ob = "Don't Quite understand"
+       
+        pr="PyBot : " + ob + "\n"
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, pr)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+        self.last_sent_label(str(time.strftime( "Last message sent: " + '%B %d, %Y' + ' at ' + '%I:%M %p')))
+        self.entry_field.delete(0,END)
+        time.sleep(0)
+        #return ob
+        
+    def font_change_default(self):
+        self.text_box.config(font="Verdana 10")
+        self.entry_field.config(font="Verdana 10")
+        self.font = "Verdana 10"
+
+    def font_change_times(self):
+        self.text_box.config(font="Times")
+        self.entry_field.config(font="Times")
+        self.font = "Times"
+
+    def font_change_system(self):
+        self.text_box.config(font="System")
+        self.entry_field.config(font="System")
+        self.font = "System"
+
+    def font_change_helvetica(self):
+        self.text_box.config(font="helvetica 10")
+        self.entry_field.config(font="helvetica 10")
+        self.font = "helvetica 10"
+
+    def font_change_fixedsys(self):
+        self.text_box.config(font="fixedsys")
+        self.entry_field.config(font="fixedsys")
+        self.font = "fixedsys"
+
+    def color_theme_default(self):
+        self.master.config(bg="#EEEEEE")
+        self.text_frame.config(bg="#EEEEEE")
+        self.entry_frame.config(bg="#EEEEEE")
+        self.text_box.config(bg="#FFFFFF", fg="#000000")
+        self.entry_field.config(bg="#FFFFFF", fg="#000000", insertbackground="#000000")
+        self.send_button_frame.config(bg="#EEEEEE")
+        self.send_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
+        #self.emoji_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
+        self.sent_label.config(bg="#EEEEEE", fg="#000000")
+
+        self.tl_bg = "#FFFFFF"
+        self.tl_bg2 = "#EEEEEE"
+        self.tl_fg = "#000000"
+
+    # Dark
+    def color_theme_dark(self):
+        self.master.config(bg="#2a2b2d")
+        self.text_frame.config(bg="#2a2b2d")
+        self.text_box.config(bg="#212121", fg="#FFFFFF")
+        self.entry_frame.config(bg="#2a2b2d")
+        self.entry_field.config(bg="#212121", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#2a2b2d")
+        self.send_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
+       # self.emoji_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#2a2b2d", fg="#FFFFFF")
+
+        self.tl_bg = "#212121"
+        self.tl_bg2 = "#2a2b2d"
+        self.tl_fg = "#FFFFFF"
+
+    # Grey
+    def color_theme_grey(self):
+        self.master.config(bg="#444444")
+        self.text_frame.config(bg="#444444")
+        self.text_box.config(bg="#4f4f4f", fg="#ffffff")
+        self.entry_frame.config(bg="#444444")
+        self.entry_field.config(bg="#4f4f4f", fg="#ffffff", insertbackground="#ffffff")
+        self.send_button_frame.config(bg="#444444")
+        self.send_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
+        #self.emoji_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
+        self.sent_label.config(bg="#444444", fg="#ffffff")
+
+        self.tl_bg = "#4f4f4f"
+        self.tl_bg2 = "#444444"
+        self.tl_fg = "#ffffff"
+
+
+    def color_theme_turquoise(self):
+        self.master.config(bg="#003333")
+        self.text_frame.config(bg="#003333")
+        self.text_box.config(bg="#669999", fg="#FFFFFF")
+        self.entry_frame.config(bg="#003333")
+        self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#003333")
+        self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#003333", fg="#FFFFFF")
+
+        self.tl_bg = "#669999"
+        self.tl_bg2 = "#003333"
+        self.tl_fg = "#FFFFFF"    
+
+    # Blue
+    def color_theme_dark_blue(self):
+        self.master.config(bg="#263b54")
+        self.text_frame.config(bg="#263b54")
+        self.text_box.config(bg="#1c2e44", fg="#FFFFFF")
+        self.entry_frame.config(bg="#263b54")
+        self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#263b54")
+        self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#263b54", fg="#FFFFFF")
+
+        self.tl_bg = "#1c2e44"
+        self.tl_bg2 = "#263b54"
+        self.tl_fg = "#FFFFFF"
+
+ 
+    
+
+    # Torque
+    def color_theme_turquoise(self):
+        self.master.config(bg="#003333")
+        self.text_frame.config(bg="#003333")
+        self.text_box.config(bg="#669999", fg="#FFFFFF")
+        self.entry_frame.config(bg="#003333")
+        self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#003333")
+        self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#003333", fg="#FFFFFF")
+
+        self.tl_bg = "#669999"
+        self.tl_bg2 = "#003333"
+        self.tl_fg = "#FFFFFF"
+
+    # Hacker
+    def color_theme_hacker(self):
+        self.master.config(bg="#0F0F0F")
+        self.text_frame.config(bg="#0F0F0F")
+        self.entry_frame.config(bg="#0F0F0F")
+        self.text_box.config(bg="#0F0F0F", fg="#33FF33")
+        self.entry_field.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
+        self.send_button_frame.config(bg="#0F0F0F")
+        self.send_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#0F0F0F", fg="#33FF33")
+
+        self.tl_bg = "#0F0F0F"
+        self.tl_bg2 = "#0F0F0F"
+        self.tl_fg = "#33FF33"
+
+    
+
+    # Default font and color theme
+    def default_format(self):
+        self.font_change_default()
+        self.color_theme_default()    
+
+        
+root=Tk()
+
+f.close()
+a = ChatInterface(root)
+root.geometry(window_size)
+root.title("PyBot")
+root.mainloop()
